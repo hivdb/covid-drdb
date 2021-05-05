72c2f2dc4840d1b1d622a0e9ab3dd43666cddf18
Philip Tzou <philip.npc@gmail.com>
2021-05-04 18:07:58 -0700

    Add constraint on rx_conv_plasma.infection

--- constraints.sql
+++ constraints.sql
@@ -1,5 +1,20 @@
+-- In susc_results, one of fold_cmp/fold or resistance_level should not be NULL
 ALTER TABLE susc_results
   ADD CONSTRAINT chk_fold_resistance_level CHECK (
-    (fold_cmp IS NOT NULL AND fold IS NOT NULL AND resistance_level IS NULL) OR -- case 1, fold_cmp/fold are presented
-    (fold_cmp IS NULL AND fold IS NULL AND resistance_level IS NOT NULL)        -- case 2, resistance_level is presented
+    (
+      -- case 1, fold_cmp/fold are presented
+      fold_cmp IS NOT NULL AND
+      fold IS NOT NULL AND
+      resistance_level IS NULL
+    ) OR
+    (
+      -- case 2, resistance_level is presented
+      fold_cmp IS NULL AND
+      fold IS NULL AND
+      resistance_level IS NOT NULL
+    )
   );
+
+-- rx_conv_plasma.infection can be NULL but not empty string
+ALTER TABLE rx_conv_plasma
+  ADD CONSTRAINT chk_infection CHECK (infection != '');
