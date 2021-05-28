23254314f791f81b8842811247c016c5dc59c563
Philip Tzou <philip.npc@gmail.com>
2021-05-28 15:55:25 -0700

    Customize db_to_sqlite script to add index and remove foreign keys

--- scripts/make-slim-version.sh
+++ scripts/make-slim-version.sh
@@ -3,7 +3,11 @@
 DB_FILE=$1
 
 sqlite3 $DB_FILE <<EOF
+DELETE FROM rx_plasma;
+DELETE FROM subject_history;
 DELETE FROM antibody_articles;
+DELETE FROM antibody_epitopes;
 DELETE FROM dms_escape_results;
+DELETE FROM dms_ace2_binding;
 VACUUM
 EOF
