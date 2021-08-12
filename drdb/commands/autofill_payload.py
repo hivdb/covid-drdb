b8d1cf92f4ab6bf5a5705b5638985fa4a8da3387
Kaiming <mads396@gmail.com>
2021-08-12 14:22:27 -0700

    rename birthday to birth_day

--- drdb/commands/autofill_payload.py
+++ drdb/commands/autofill_payload.py
@@ -220,7 +220,7 @@ def autofill_subjects(tables_dir):
             'ref_name',
             'subject_name',
             'subject_species',
-            'birthday',
+            'birth_year',
             'num_subjects'
         ],
         BOM=True
