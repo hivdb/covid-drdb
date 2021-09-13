24c793d3f69edcc485b9f54150e85d59cf9696d4
Philip Tzou <philip.npc@gmail.com>
2021-09-13 16:48:58 -0700

    Fix event_date comparison for dose 2 and 3 too

--- derived_tables/02_rx_vacc_plasma.sql
+++ derived_tables/02_rx_vacc_plasma.sql
@@ -42,7 +42,7 @@ INSERT INTO rx_vacc_plasma
       WHERE
         SH.ref_name=RX.ref_name AND
         SH.subject_name=RX.subject_name AND
-        SH.event_date<=RX.collection_date AND
+        SH.event_date<RX.collection_date AND
         SH.vaccine_name IS NOT NULL
     ) AS vaccine_name,
     PTH_isolation.location,
@@ -75,7 +75,7 @@ INSERT INTO rx_vacc_plasma
       WHERE
         SH.ref_name=RX.ref_name AND
         SH.subject_name=RX.subject_name AND
-        SH.event_date<=RX.collection_date AND
+        SH.event_date<RX.collection_date AND
         SH.vaccine_name IS NOT NULL
     ) AS vaccine_name,
     PTH_isolation.location,
