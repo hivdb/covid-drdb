0847a0c904cef4e8e47f46824e395e5ba8dee206
Kaiming <mads396@gmail.com>
2021-10-02 08:21:51 -0700

    drop views from slim version

--- scripts/export-sqlite.sh
+++ scripts/export-sqlite.sh
@@ -17,6 +17,7 @@ echo "build/covid-drdb-latest.db -> covid-drdb-$VERSION.db"
 
 cp /local/covid-drdb-$VERSION.db /local/covid-drdb-$VERSION-slim.db
 ./scripts/make-slim-version.sh /local/covid-drdb-$VERSION-slim.db
+sqlite3 /local/covid-drdb-$VERSION-slim.db < drop_views.sql
 echo "Written build/covid-drdb-$VERSION-slim.db"
 # cp -v local/covid-drdb-$VERSION-slim.db ../chiro-cms/downloads/covid-drdb/$VERSION-slim.db
 # rm ../chiro-cms/downloads/covid-drdb/latest-slim.db 2>/dev/null || true
