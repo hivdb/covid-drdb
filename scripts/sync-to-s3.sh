d7c465af71e07fc007752d2fbbff84f5d183fd96
Philip Tzou <philip.npc@gmail.com>
2022-04-08 13:17:23 -0700

    Add accession token to sync-to-s3.sh for bypassing Github rate limit

--- scripts/sync-to-s3.sh
+++ scripts/sync-to-s3.sh
@@ -37,22 +37,21 @@ for i in $(seq 0 $((num_releases - 1))); do
     if ! find_and_pop_s3_file "$name"; then
       echo "download: $name"
       # remote file doesn't exist
-      real_url=$(curl -sSL $url | jq -r ".browser_download_url")
-      curl -sSL $real_url -o $name
+      real_url=$(curl -sSL -H "Authorization: $GITHUB_TOKEN" $url | jq -r ".browser_download_url")
+      curl -sSL -H "Authorization: $GITHUB_TOKEN" $real_url -o $name
       echo "compress: $name"
       pigz -9 $name
       mv $name.gz $name
+      aws s3 sync /github-assets/ "${S3_BUCKET}/${S3_PREFIX}/" \
+        --content-encoding gzip --cache-control max-age=2592000
     fi
   done
 done
 
 popd >/dev/null
 
-aws s3 sync /github-assets/ "${S3_BUCKET}/${S3_PREFIX}/" \
-  --content-encoding gzip --cache-control max-age=2592000
-
-echo "$s3_files" | while read file; do
-  if [[ $file != "" ]]; then
-    aws s3 rm ${S3_BUCKET}/${S3_PREFIX}/${file}
-  fi
-done
+# echo "$s3_files" | while read file; do
+#   if [[ $file != "" ]]; then
+#     aws s3 rm ${S3_BUCKET}/${S3_PREFIX}/${file}
+#   fi
+# done
